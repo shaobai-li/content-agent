91735ec0815df44d4a0760a90ee838e67d852b5a
shaobai-li <shaobai.ai@gmail.com>
2026-02-04 00:10:49 +0800

    refactor(frontend_agent-nm): migrate page to shared AgentPageLayout

--- frontend/app/agent_nm/page.tsx
+++ frontend/app/agent_nm/page.tsx
@@ -1,33 +1,15 @@
 "use client"
 
-import { useChat } from "@/hooks/useChat"
-import { ChatMessage } from "@/components/features/chat/ChatMessage"
-import { ChatInput } from "@/components/features/chat/ChatInput"
+import { AgentPageLayout } from "@/components/layout/AgentPageLayout"
 import { DataPanel } from "@/components/features/data/DataPanel"
 import { DataHeader } from "@/components/features/data/DataHeader"
-import { ChatHeader } from "@/components/features/chat/ChatHeader"
 
 export default function AgentNmPage() {
-	const { input, setInput, messages, handleSend } = useChat({ agentId: "note_manager" })
 	return (
-		<div className="h-full flex flex-grow flex-row">
-			<div className="flex-1 flex flex-col">
-				<DataHeader />
-				<div className="flex-1 flex flex-col p-4 border bg-neutral-50">
-					<DataPanel />
-				</div>
-			</div>
-			<div className="w-100 flex flex-col flex-none">
-				<ChatHeader />
-				<div className="flex-1 flex flex-col">
-					<div className="flex-1 flex flex-col border p-4 bg-neutral-50">
-						<ChatMessage messages={messages} />
-					</div>
-					<div className="flex flex-col border p-4 bg-background">
-						<ChatInput value={input} onChange={setInput} onSend={handleSend} />
-					</div>
-				</div>
-			</div>
-		</div>
+		<AgentPageLayout 
+			agentId="note_manager"
+			leftHeader={<DataHeader />}
+			leftBody={<DataPanel />}
+		/>
 	)
 }
