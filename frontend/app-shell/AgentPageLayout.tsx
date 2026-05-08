a7a0312b6434264de36ed4ce87f48988ae2a03ef
wyp512 <wyp1137667201@gmail.com>
2026-05-08 19:45:28 +0800

    fix(settings): 修复设置页在左侧布局内溢出无法滚动并调高文本域默认高度

--- frontend/app-shell/AgentPageLayout.tsx
+++ frontend/app-shell/AgentPageLayout.tsx
@@ -35,7 +35,7 @@ function AgentPageLayoutInner({ leftHeader, leftBody, rightBody }: AgentPageLayo
                 <div className="flex h-16 px-8 border bg-card shrink-0">
                     {leftHeader}
                 </div>
-                <div className="flex-1 flex flex-col p-6 border bg-neutral-50">
+                <div className="flex min-h-0 flex-1 flex-col p-6 border bg-neutral-50">
                     {leftBody}
                 </div>
             </div>
